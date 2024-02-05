d24d9a0fcb0f194d239a7f8e7dca206fe6824084
WangSyutung <wangxudong@yjut.edu,kg>
2024-02-05 13:05:24 +0800

    update little pair

--- app/src/main/java/kg/edu/yjut/morseinputmethod/view/OpenSourceLicenseActivity.kt
+++ app/src/main/java/kg/edu/yjut/morseinputmethod/view/OpenSourceLicenseActivity.kt
@@ -1,12 +1,21 @@
 package kg.edu.yjut.morseinputmethod.view
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kg.edu.yjut.morseinputmethod.R
 
 class OpenSourceLicenseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_open_source_license)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        // 设置透明导航栏
+        window.navigationBarColor = Color.TRANSPARENT
+
+        // 隐藏标题栏
+        supportActionBar?.title = "开放源代码许可"
+
     }
 }
--- app/src/main/java/kg/edu/yjut/morseinputmethod/view/WebActivity.kt
+++ app/src/main/java/kg/edu/yjut/morseinputmethod/view/WebActivity.kt
@@ -1,14 +1,22 @@
 package kg.edu.yjut.morseinputmethod.view
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import kg.edu.yjut.morseinputmethod.R
 
 class WebActivity : AppCompatActivity() {
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        // 设置透明导航栏
+        window.navigationBarColor = Color.TRANSPARENT
+        // 隐藏标题栏
         // 读取传入的参数 url
         var url = intent.getStringExtra("url")
         // 打开网页
@@ -17,7 +25,7 @@ class WebActivity : AppCompatActivity() {
             // 启用 JavaScript
             web.settings.javaScriptEnabled = true
             web.loadUrl(url)
-
+            supportActionBar?.title = web.title
         }
 
     }
